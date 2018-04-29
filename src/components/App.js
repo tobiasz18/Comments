67be47b634ad4fce3ef612c9bea8e5d8540e00b8
Tobiasz18 <tobiaszpost@gmail.com>
2018-04-29 11:53:57 +0200

    webpack

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../style.scss'
+const App = () => {
+    return (
+        <div className="container">
+            <p>React HereRLo</p>
+        </div>
+    )
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
             }
+
         ]
-    }
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]
 };
